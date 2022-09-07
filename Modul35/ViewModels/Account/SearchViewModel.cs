5e09db60beee721122865b8fb53b102368c396c4
Nikolyase <96522347+Nikolyase@users.noreply.github.com>
2022-09-07 22:34:24 +0300

    Добавьте файлы проекта.

---
+++ Modul35/ViewModels/Account/SearchViewModel.cs
@@ -0,0 +1,15 @@
+﻿using AwesomeNetwork.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomeNetwork.ViewModels.Account
+{
+    public class SearchViewModel
+    {
+        public List<UserWithFriendExt> UserList { get; set; }
+
+        //public List<User> UserList { get; set; }
+    }
+}
